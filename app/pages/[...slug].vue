b2a598ae11a9ffddb45cff31693b831c76f49be6
Liam <liam3006x@gmail.com>
2026-01-28 17:42:54 +0000

    Update coming soon bg glow

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="h-screen flex flex-col items-center justify-center relative overflow-hidden">
+    <div class="min-h-screen flex flex-col items-center justify-center relative overflow-hidden">
         <div
             class="absolute w-[100vw] h-[914px] -top-[600px] bg-[radial-gradient(closest-side,white,transparent)] opacity-20 blur-[100px] transform-gpu"
         />
 
         <div
-            class="absolute w-[100vw] h-[914px] -bottom-[600px] bg-[radial-gradient(closest-side,var(--color-primary),transparent)] opacity-20 blur-[100px] transform-gpu"
+            class="absolute w-[100vw] h-[914px] bottom-[-500px] bg-[radial-gradient(closest-side,var(--color-primary),transparent)] opacity-20 blur-[100px] transform-gpu"
         />
 
         <div class="container mx-auto flex flex-col items-center justify-center">
