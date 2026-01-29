8f084ff5d23b27ae24ef3bf5b077442bfdd86d05
Liam <liam3006x@gmail.com>
2026-01-29 19:49:32 +0000

    Add HeroWithCTA

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,23 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import HeroWithCta from '~/components/project/heroes/HeroWithCta.vue';
+</script>
 
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center relative overflow-hidden">
-        <div
-            class="absolute w-[100vw] h-[914px] -top-[600px] bg-[radial-gradient(closest-side,white,transparent)] opacity-20 blur-[100px] transform-gpu"
+    <div class="flex flex-col">
+        <HeroWithCta
+            heading="Crafting Digital Experiences That Define the Future"
+            subheading="DIGITAL PRODUCT AGENCY"
+            :primary-cta="{
+                label: 'Get Started',
+                href: '#'
+            }"
+            :secondary-cta="{
+                label: 'Our Work',
+                href: '#'
+            }"
         />
-
-        <div
-            class="absolute w-[100vw] h-[914px] bottom-[-500px] bg-[radial-gradient(closest-side,var(--color-primary),transparent)] opacity-20 blur-[100px] transform-gpu"
-        />
-
-        <div class="container mx-auto flex flex-col items-center justify-center">
-            <h1 class="text-3xl flex flex-row relative">
-                <span class="text-primary font-bold">LXST</span>
-                <span>.digital</span>
-            </h1>
-
-            <h2 class="mt-2 text-foreground/50">Coming Soon...</h2>
-        </div>
     </div>
 </template>
 
